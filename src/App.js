a1db260f3f57a1bcb12822b03622aae4bf8ae953
Nwachukwu Francis <francisnwachukwu100@gmail.com>
2022-10-15 16:31:11 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import { Download, Features, SectionWrapper } from "./components";
+import assets from "./assets";
+import styles from "./styles/Global";
+
+const App = () => {
+  return (
+    <>
+      <SectionWrapper
+        title="Your own store of Nifty NFTs. Start selling & Growing."
+        description="Buy, store, collect NFTs, exchange & earn crypto. Join 25+ million people using OpenBlock Marketplace."
+        showBtn
+        mockupImg={assets.homeHero}
+        banner="banner"
+      />
+      <SectionWrapper
+        title="Smart User Interface Marketplace"
+        description="Experience a buttery UI of OpenBlock NFT Marketplace. Smooth constant colors of a fluent UI design."
+        mockupImg={assets.homeCards}
+        reverse
+      />
+      <Features />
+      <SectionWrapper
+        title="Deployment"
+        description="OpenBlock is built using Expo which run natively on all users' devices. You can easily get your app into people's hands."
+        mockupImg={assets.feature}
+        reverse
+      />
+      <SectionWrapper
+        title="Creative way to showcase the store"
+        description="The app contains two screens. The first screen lists all NFts while the second one shows the details of a specific NFT."
+        mockupImg={assets.mockup}
+        banner="banner02"
+      />
+      <Download />
+
+      <div className="px-4 py-2 justify-center items-center bg-primary flex-col text-center banner04">
+        <p className={`${styles.pText} ${styles.whiteText}`}>
+          Made with love by <span className="bold">devFrancis</span>
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default App;
